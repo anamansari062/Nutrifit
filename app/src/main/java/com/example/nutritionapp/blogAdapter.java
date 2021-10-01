32f98a66d2ece87dfcb9a24102440c14080ed26c
Anam Ansari <anamansari062@gmail.com>
2021-10-01 11:00:48 +0530

    Blog recycler view created

---
+++ app/src/main/java/com/example/nutritionapp/blogAdapter.java
@@ -0,0 +1,39 @@
+package com.example.nutritionapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class blogAdapter extends RecyclerView.Adapter<blogAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public blogAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater= LayoutInflater.from(parent.getContext());
+        View view= layoutInflater.inflate(R.layout.blog_prototype, parent, false);
+        ViewHolder viewHolder=  new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull blogAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        Button blogLink;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            blogLink= itemView.findViewById(R.id.blog_link_b);
+        }
+    }
+}
--- app/src/main/java/com/example/nutritionapp/ui/blog/BlogViewModel.java
+++ app/src/main/java/com/example/nutritionapp/ui/blog/BlogViewModel.java
@@ -3,14 +3,21 @@ package com.example.nutritionapp.ui.blog;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.nutritionapp.R;
+import com.example.nutritionapp.blogAdapter;
 
 public class BlogViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
+
     public BlogViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is Blog fragment");
+         mText = new MutableLiveData<>();
+         mText.setValue("Blogs Available");
+
     }
 
     public LiveData<String> getText() {
