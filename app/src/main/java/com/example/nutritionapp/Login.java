bfd4246910b319585234dc957c68c7b174735ed5
akki2021 <1032200306@tcetmumbai.in>
2021-10-15 01:35:46 +0530

    complict

--- app/src/main/java/com/example/nutritionapp/Login.java
+++ app/src/main/java/com/example/nutritionapp/Login.java
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
