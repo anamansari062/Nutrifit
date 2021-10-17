8e194ad17d00cbbca00aa99bd54d9204349eb7ec
akki2021 <1032200306@tcetmumbai.in>
2021-10-17 20:44:26 +0530

    login page

--- app/src/main/java/com/example/nutritionapp/Login.java
+++ app/src/main/java/com/example/nutritionapp/Login.java
@@ -24,7 +24,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 
 public class Login extends AppCompatActivity {
