77aad0692caddace7e7ea1e4dc79c3682209bebf
cdhiraj40 <chauhandhiraj40@gmail.com>
2021-11-17 16:12:11 +0530

    final changes

--- app/src/main/java/com/example/nutritionapp/ui/blog/BlogFragment.java
+++ app/src/main/java/com/example/nutritionapp/ui/blog/BlogFragment.java
@@ -7,9 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,37 +16,36 @@ import com.example.nutritionapp.R;
 import com.example.nutritionapp.adapters.BlogAdapter;
 import com.example.nutritionapp.databinding.FragmentBlogBinding;
 
+import java.util.ArrayList;
+
 
 public class BlogFragment extends Fragment {
 
-    private BlogViewModel blogViewModel;
     private FragmentBlogBinding binding;
     RecyclerView blog_recycler;
     BlogAdapter blog_adapter;
+    TextView blogLink;
+    ArrayList<String> blogLinks = new ArrayList<>();
 
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        blogViewModel = new ViewModelProvider(this).get(BlogViewModel.class);
+        BlogViewModel blogViewModel = new ViewModelProvider(this).get(BlogViewModel.class);
 
         binding = FragmentBlogBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         blog_recycler = root.findViewById(R.id.blogRecycler);
+        blogLink= root.findViewById(R.id.blog_link);
         blog_adapter = new BlogAdapter();
         blog_recycler.setLayoutManager(new LinearLayoutManager(getContext()));
         blog_recycler.setAdapter(blog_adapter);
 
         final TextView textView = binding.textBlog;
-        blogViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-          return root;
-        }
+        blogViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
 
     @Override
     public void onDestroyView() {
