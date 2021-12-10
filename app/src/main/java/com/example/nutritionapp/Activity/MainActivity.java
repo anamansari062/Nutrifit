05d6ae50ddc26d09d8e99c6bcb17ced47b1f2e06
ishir <ishiragrawal02@gmail.com>
2021-12-10 20:12:07 +0530

    some important changes

--- app/src/main/java/com/example/nutritionapp/Activity/MainActivity.java
+++ app/src/main/java/com/example/nutritionapp/Activity/MainActivity.java
@@ -152,4 +152,28 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+    @Override
+    public void onBackPressed() {
+
+        new AlertDialog.Builder(this)
+                .setTitle("Really Exit?")
+                .setMessage("Are you sure you want to exit?")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        MainActivity.super.onBackPressed();
+                        quit();
+                    }
+                }).create().show();
+    }
+
+
+    public void quit() {
+        Intent start = new Intent(Intent.ACTION_MAIN);
+        start.addCategory(Intent.CATEGORY_HOME);
+        start.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        start.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(start);
+    }
 }
