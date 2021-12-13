721f935b4a583b16fc8956823d26e229a5508901
Anam Ansari <anamansari062@gmail.com>
2021-12-13 15:04:07 +0530

    Removed Blog

--- app/src/main/java/com/example/nutritionapp/Activity/MainActivity.java
+++ app/src/main/java/com/example/nutritionapp/Activity/MainActivity.java
@@ -76,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_profile, R.id.nav_calculate, R.id.nav_blog, R.id.nav_about, R.id.nav_home, R.id.nav_share)
+                R.id.nav_profile, R.id.nav_calculate, R.id.nav_about, R.id.nav_home, R.id.nav_share)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
